c678e3d35185a9a6dfbcb0157b09e27161b716e0
safarislava <safarislava@gmail.com>
2025-11-01 13:45:27 +0300

    Add error message

--- PointChecker/point-checker-app/src/pages/start-page/form.component/form.component.ts
+++ PointChecker/point-checker-app/src/pages/start-page/form.component/form.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {ChangeDetectorRef, Component} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AbstractControl, FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
@@ -14,11 +14,12 @@ import { Router } from '@angular/router';
 export class FormComponent {
   public isCompliant = false;
   public loginForm!: FormGroup;
+  public errorMessage: boolean = false;
 
   private isEnterOrRegister: boolean = true;
-  private urlApi = "http://185.239.141.48:8080/PointChecker-1.0/api/user";
+  private urlApi = "http://localhost:8080/PointChecker-1.0/api/user"; //185.239.141.48
 
-  constructor(private router: Router, private http: HttpClient, private formBuilder: FormBuilder) {
+  constructor(private router: Router, private http: HttpClient, private formBuilder: FormBuilder, private cdr: ChangeDetectorRef) {
     this.loginForm = this.createForm();
   }
 
@@ -35,13 +36,21 @@ export class FormComponent {
 
   public openForm(): void {
     this.isCompliant = true;
+    this.errorMessage = false;
   }
 
   public closeForm(): void {
     this.isCompliant = false;
+    this.errorMessage = false;
   }
 
   public onSubmit(): void {
+    this.loginForm.markAllAsTouched();
+
+    if (this.loginForm.invalid) {
+      return;
+    }
+
     const payload = {
       username: this.username?.value,
       password: this.password?.value,
@@ -53,10 +62,12 @@ export class FormComponent {
       })
         .subscribe({
           next: (result) => {
+            this.errorMessage = false;
             this.router.navigate(['/main']);
           },
           error: (error) => {
-            console.log('Login failed:', error.status);
+            this.errorMessage = true;
+            this.cdr.detectChanges();
           }
         });
     }
@@ -66,10 +77,12 @@ export class FormComponent {
       })
         .subscribe({
           next: (result) => {
+            this.errorMessage = false;
             this.router.navigate(['/main']);
           },
           error: (error) => {
-            console.log('Register failed:', error.status);
+            this.errorMessage = true;
+            this.cdr.detectChanges();
           }
         });
     }
@@ -77,10 +90,14 @@ export class FormComponent {
 
   public onRegistrationClick(): void {
     this.isEnterOrRegister = false;
+    this.errorMessage = false;
+    this.onSubmit();
   }
 
   public onEnterClick(): void {
     this.isEnterOrRegister = true;
+    this.errorMessage = false;
+    this.onSubmit();
   }
 
   get username(): AbstractControl | null { return this.loginForm.get('username'); }
