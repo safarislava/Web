2af8e632e2c027ec2d10f7c05170f218ff107192
safarislava <safarislava@gmail.com>
2025-11-10 13:02:01 +0300

    Some changes

--- PointChecker/PointCheckerApi/src/main/java/ru/ifmo/se/api/services/ShotService.java
+++ PointChecker/PointCheckerApi/src/main/java/ru/ifmo/se/api/services/ShotService.java
@@ -8,6 +8,7 @@ import ru.ifmo.se.api.dto.requests.ShotRequest;
 import ru.ifmo.se.api.dto.responses.ShotResponse;
 import ru.ifmo.se.api.entities.Shot;
 import ru.ifmo.se.api.entities.User;
+import ru.ifmo.se.api.exceptions.BadRequestException;
 import ru.ifmo.se.api.repositories.ShotRepository;
 import ru.ifmo.se.api.repositories.UserRepository;
 import ru.ifmo.se.api.entities.*;
@@ -24,33 +25,31 @@ public class ShotService {
     private final UserRepository userRepository;
     private final ValidatorService validatorService;
 
-    public List<ShotResponse> getShotResponses(String username) {
+    private User getUser(String username) {
         Optional<User> user = userRepository.findByUsername(username);
-        if (user.isEmpty()) throw new IllegalArgumentException("User not found");
+        if (user.isEmpty()) throw new BadRequestException("User not found");
+        return user.get();
+    }
 
-        List<Shot> shots = shotRepository.findAllByUser(user.get());
+    public List<ShotResponse> getShotResponses(String username) {
+        User user = getUser(username);
+        List<Shot> shots = shotRepository.findAllByUser(user);
         List<ShotResponse> shotResponses = new ArrayList<>();
         shots.forEach(shot -> shotResponses.add(new ShotResponse(shot)));
         return shotResponses;
     }
 
     public void addShot(ShotRequest request, String username) {
-        Optional<User> user = userRepository.findByUsername(username);
-        if (user.isEmpty()) throw new IllegalArgumentException("User not found");
-
+        User user = getUser(username);
         validatorService.validate(request);
-
         RequestProcessor processor = getRequestProcessor(request);
-        Shot shot = processor.process(request, user.get());
-
+        Shot shot = processor.process(request, user);
         shotRepository.save(shot);
     }
 
     public void clearShots(String username) {
-        Optional<User> user = userRepository.findByUsername(username);
-        if (user.isEmpty()) throw new IllegalArgumentException("User not found");
-
-        shotRepository.deleteAllByUser(user.get());
+        User user = getUser(username);
+        shotRepository.deleteAllByUser(user);
     }
 
     private RequestProcessor getRequestProcessor(ShotRequest request) {
