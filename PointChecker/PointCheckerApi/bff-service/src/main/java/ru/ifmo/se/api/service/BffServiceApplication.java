d575faf5b14bbf2646aba97fe81fb17d9d2cd522
safarislava <safarislava@gmail.com>
2025-11-21 18:42:00 +0300

    Add BFF Service

--- PointChecker/PointCheckerApi/bff-service/src/main/java/ru/ifmo/se/api/service/BffServiceApplication.java
+++ PointChecker/PointCheckerApi/bff-service/src/main/java/ru/ifmo/se/api/service/BffServiceApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class UserServiceApplication {
+public class BffServiceApplication {
     public static void main(String[] args) {
-        SpringApplication.run(UserServiceApplication.class, args);
+        SpringApplication.run(BffServiceApplication.class, args);
     }
 }
