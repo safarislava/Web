11057e2b42e4d3ef19d8706191723ac623987bad
safarislava <49348863+safarislava@users.noreply.github.com>
2025-12-27 11:47:54 +0300

    Delete Lab1 directory

--- Lab1/httpd-root/run.sh
+++
@@ -1,2 +0,0 @@
-apachectl -f ~/httpd-root/httpd-hell.conf
-java -classpath /home/studs/s467570/httpd-root/fcgi-bin/fastcgi-lib.jar -DFCGI_PORT=24571 -jar /home/studs/s467570/httpd-root/fcgi-bin/fastcgi-server.jar
